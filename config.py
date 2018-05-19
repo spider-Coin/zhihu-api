075edb5b3c16168abeb91407ea4b446fa034a5d2
gaojin <1019855680@qq.com>
2018-05-20 04:29:49 +0800

    celery config

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 from datetime import timedelta
 
-
-# MONGO_URI = 'mongodb://dapdap:dapdapmima123@172.31.135.89:27017/dapdap'
+CELERYBEAT_SCHEDULE = {
+    'every-minute': {
+        'task': 'app.upLastime',
+        'schedule': timedelta(seconds=60 * 30)
+    }
+}
 MONGO_URI = 'mongodb://127.0.0.1:27017/zhihu'
 CELERY_BROKER_URL = 'redis://localhost:6379',
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CACHE_REDIS_HOST = '127.0.0.1'
 CACHE_REDIS_PORT = 6379
 CACHE_REDIS_DB = 1
-
